aa721234156a1e5182f208d6c34acf82c6e227be
Bryan <bryanryu1@gmail.com>
2021-01-19 16:40:52 -0500

    added animations to pages

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,55 +1,49 @@
 import React from "react"
+import Fade from "react-reveal/Fade"
+
 import Image from "../components/image"
 import LinkedIn from "../components/linkedIn"
 import Gmail from "../components/gmail"
 
 const Contact = () => (
-  <div style={{
-    minHeight: '20rem',
-    paddingTop: '2rem',
-    paddingBottom: '3rem',
-  }}>
-    <h1
-      style={{
-        color: "#1E7CD9",
-      }}
-      className='titles'
-    >
-      con·tact
-    </h1>
-    <p
-    style={{paddingTop: '1.5rem', paddingBottom: '2.5rem', textAlign: 'center'}}>
-      I'd love to link!
-      <br></br>
-      You can contact me at any of the channels below.
-    </p>
-  
   <div
     style={{
-      justifyContent: "space-evenly",
-      display: "flex",
-      alignItems: 'center',
-      paddingBottom: '10rem'
+      minHeight: "20rem",
+      paddingTop: "2rem",
+      paddingBottom: "3rem",
     }}
   >
-    <a target="_blank" href="https://github.com/bryanryu1" rel="noreferrer">
-      <Image />
-    </a>
-    <a
-      target="_blank"
-      href="https://www.linkedin.com/in/bryan-ryu/"
-      rel="noreferrer"
-    >
-      <LinkedIn />
-    </a>
-    <a
-      target="_blank"
-      href="https://mail.google.com/mail/?view=cm&fs=1&to=bryanryu1@gmail.com&su=Hello!"
-      rel="noreferrer"
-    >
-      <Gmail />
-    </a>
-  </div>
+    <Fade top>
+      <h1 className="titles">con·tact</h1>
+    </Fade>
+    <Fade>
+      <div className="contactText">
+        I'd love to link!
+        <br></br>
+        You can contact me at any of the channels below.
+      </div>
+    </Fade>
+    <Fade>
+      <div className="contactLinks">
+        <a target="_blank" href="https://github.com/bryanryu1" rel="noreferrer">
+          <Image />
+        </a>
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/bryan-ryu/"
+          rel="noreferrer"
+        >
+          <LinkedIn />
+        </a>
+        <a
+          target="_blank"
+          href="https://mail.google.com/mail/?view=cm&fs=1&to=bryanryu1@gmail.com&su=Hello!"
+          rel="noreferrer"
+        >
+          <Gmail />
+        </a>
+      </div>
+    </Fade>
   </div>
 )
 
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Fade from "react-reveal/Fade"
 
 import Cereal from "../components/cereal"
 import Water from "../components/water"
@@ -10,96 +11,108 @@ const Portfolio = () => (
       minHeight: "20rem",
     }}
   >
-    <h1
-      style={{
-        color: "#1E7CD9",
-        paddingTop: "7rem",
-      }}
-      className="titles"
-    >
-      port·fo·li·o
-    </h1>
+    <Fade top>
+      <h1
+        style={{
+          color: "#1E7CD9",
+        }}
+        className="titles"
+      >
+        port·fo·li·o
+      </h1>
+    </Fade>
 
     <div className="skillsGrid">
       <div className="first-skill-area">
-        <h3 className="skillHead">My Skills</h3>
-        <div className="skills">
-          <div className="proficiency">Proficient:</div>
-          <div className="skillWrap">
-            <ul>
-              <li>Javascript</li>
-              <li>React</li>
-              <li>React Native</li>
-              <li>Redux</li>
-              <li>Express</li>
-            </ul>
-            <ul>
-              <li>HTML5</li>
-              <li>CSS</li>
-              <li>Git</li>
-              <li>Node.js</li>
-              <li>Sequelize</li>
-            </ul>
+        <Fade top>
+          <h3 className="skillHead">My Skills</h3>
+        </Fade>
+        <Fade>
+          <div className="skills">
+            <div className="proficiency">Proficient:</div>
+            <div className="skillWrap">
+              <ul>
+                <li>Javascript</li>
+                <li>React</li>
+                <li>React Native</li>
+                <li>Redux</li>
+                <li>Express</li>
+              </ul>
+              <ul>
+                <li>HTML5</li>
+                <li>CSS</li>
+                <li>Git</li>
+                <li>Node.js</li>
+                <li>Sequelize</li>
+              </ul>
+            </div>
+            <div className="proficiency">Knowledgeable:</div>
+            <div className="skillWrap">
+              <ul>
+                <li>Ruby</li>
+                <li>Gatsby</li>
+                <li>Mocha</li>
+                <li>Chai</li>
+                <li>Jasmine</li>
+              </ul>
+            </div>
           </div>
-          <div className="proficiency">Knowledgeable:</div>
-          <div className="skillWrap">
-            <ul>
-              <li>Ruby</li>
-              <li>Gatsby</li>
-              <li>Mocha</li>
-              <li>Chai</li>
-              <li>Jasmine</li>
-            </ul>
-          </div>
-        </div>
+        </Fade>
       </div>
       <div className="second-skill-area">
-        <h3 className="projectHead">Projects</h3>
-        <div className="projectWrap">
-          <div className="project">
-            <a
-              target="_blank"
-              href="https://github.com/Rice-Krispy-Grace-Shopper/Grace-Shopper"
-              rel="noreferrer"
-            >
-              <div className='post'>
-              <Cereal />
-              <h4 className="postTitle">Cereal Emporium</h4>
-              <p className="postDesc">
-                Cereal Emporium is an e-commerce site that doesn't actually sell any cereal...
-              </p>
-              </div>
-            </a>
-          </div>
+        <Fade top>
+          <h3 className="projectHead">Projects</h3>
+        </Fade>
+        <Fade>
+          <div className="projectWrap">
+            <div className="project">
+              <a
+                target="_blank"
+                href="https://github.com/Rice-Krispy-Grace-Shopper/Grace-Shopper"
+                rel="noreferrer"
+              >
+                <div className="post">
+                  <Cereal />
+                  <h4 className="postTitle">Cereal Emporium</h4>
+                  <p className="postDesc">
+                    Cereal Emporium is an e-commerce site that doesn't actually
+                    sell any cereal...
+                  </p>
+                </div>
+              </a>
+            </div>
 
-          <div className="project">
-            <a
-              target="_blank"
-              href="https://github.com/bryanryu1/WaterApp"
-              rel="noreferrer"
-            >
-              <Water />
-              <h4 className="postTitle">WaterMon</h4>
-              <p className="postDesc">
-                Watermon is a React Native mobile application which a user can track the water they intake throughout the day.
-              </p>
-            </a>
-          </div>
+            <div className="project">
+              <a
+                target="_blank"
+                href="https://github.com/bryanryu1/WaterApp"
+                rel="noreferrer"
+              >
+                <Water />
+                <h4 className="postTitle">WaterMon</h4>
+                <p className="postDesc">
+                  Watermon is a React Native mobile application which a user can
+                  track the water they intake throughout the day.
+                </p>
+              </a>
+            </div>
 
-          <div className="project">
-            <a
-              target="_blank"
-              href="https://github.com/Do-Jest-While-True/cooked"
-              rel="noreferrer"
-            >
-              <Cooked />
-              <h4 className="postTitle">Cook'd</h4>
-              <p className="postDesc">
-                Cook'd is a mobile social media application focused on recipes. Built using React Native.
-              </p>
-            </a>
+            <div className="project">
+              <a
+                target="_blank"
+                href="https://github.com/Do-Jest-While-True/cooked"
+                rel="noreferrer"
+              >
+                <Cooked />
+                <h4 className="postTitle">Cook'd</h4>
+                <p className="postDesc">
+                  Cook'd is a mobile social media application focused on
+                  recipes. Built using React Native.
+                </p>
+              </a>
+            </div>
           </div>
-        </div>
+        </Fade>
       </div>
     </div>
   </div>
