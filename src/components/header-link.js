1a14e3a65b41d302461072460340bf201495c1b5
Bryan Ryu <bryanryu1@gmail.com>
2021-09-28 21:14:13 -0400

    react-scroll link

--- src/components/header-link.js
+++ src/components/header-link.js
@@ -1,34 +1,36 @@
 import { Link } from "gatsby"
 import React from "react"
+import Scroll from 'react-scroll'
+const ScrollLink = Scroll.ScrollLink
 
 const HeaderLink = () => (
   <nav
     className='nav'
   >
-    <Link
+    <ScrollLink
       to='#'
       className='header-item'
       >
         home
-    </Link>
-    <Link
+    </ScrollLink>
+    <ScrollLink
       to="/#about"
       className='header-item'
     >
       about
-    </Link>
-    <Link
+    </ScrollLink>
+    <ScrollLink
       to="/#portfolio"
       className='header-item'
     >
       portfolio
-    </Link>
-    <Link
+    </ScrollLink>
+    <ScrollLink
       to="/#contact"
       className='header-item'
     >
       contact
-    </Link>
+    </ScrollLink>
   </nav>
 )
 
